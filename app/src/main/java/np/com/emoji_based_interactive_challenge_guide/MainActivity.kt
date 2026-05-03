2c59d85fb9ea1a0ae694c8bda6e82d51fc9eb084
smritidhimal <dsmriti23@tbc.edu.np>
2026-05-04 02:23:16 +0545

    feat(app): initialize MainActivity with EmojiTheme and navigation

--- app/src/main/java/np/com/emoji_based_interactive_challenge_guide/MainActivity.kt
+++ app/src/main/java/np/com/emoji_based_interactive_challenge_guide/MainActivity.kt
@@ -3,8 +3,6 @@ package np.com.emoji_based_interactive_challenge_guide
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.*
 import np.com.emoji_based_interactive_challenge_guide.navigation.AppNavigation
 import np.com.emoji_based_interactive_challenge_guide.ui.theme.EmojiTheme
 
@@ -13,9 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val darkTheme = isSystemInDarkTheme()
-            
-            EmojiTheme(darkTheme = darkTheme) {
+            EmojiTheme {
                 AppNavigation()
             }
         }
